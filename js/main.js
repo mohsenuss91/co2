dc81c00671a8da5ca231304959b1d4685f4d3377
kossa <hadjikouceyla@gmail.com>
2014-05-07 21:10:01 +0100

    add Total

--- js/main.js
+++ js/main.js
@@ -1,17 +1,24 @@
 
-$(document).ready(function() {
-
+(function($){
+	
 		// Slider
-	 		$(".ex1 .mb_slider").mbSlider({
-        onSlide:function(o){
-        	$("#"+o.id+"_val").find(".val").val($(o).mbgetVal());
-        	var _this = $('#'+o.id);
-	 				calculate(_this);
-	 			},
-        onSlideLoad:function(o){
-          $("#"+o.id+"_val").find(".val").val($(o).mbgetVal());
-        }
-      });
+ 		$(".ex1 .mb_slider").mbSlider({
+      onSlide:function(o){
+      	$("#"+o.id+"_val").find(".val").val($(o).mbgetVal());
+      	var _this = $('#'+o.id);
+ 				calculate(_this);
+ 			},
+      onSlideLoad:function(o){
+        $("#"+o.id+"_val").find(".val").val($(o).mbgetVal());
+      }
+    });
+ 		// set the slider
+ 		$('.setVal input').on('input', function(event) {
+ 			var _this = $(this),
+ 					id = _this.parent().attr("id");
+				id = id.replace("_val","");
+ 			$('#'+id).mbsetVal(_this.val());
+ 		});
 
 	 	// on Change Value car
 	 	$('.setVal input').on('change keyup input', function(event) {
@@ -48,6 +55,10 @@ function calculate(_this){
  		getResult1(result1, theVal ,data1, data2, data3);		
  		var result = getResult2(result2, theVal, data1, data3, data5);		
  		getResult3(result3, result ,data6);		
+
+ 		// Set the Total
+ 		setTotal();
+
 }
 
 
@@ -63,10 +74,38 @@ function getResult2(result2, x, y, z, a){
 	result2.val(result); 
 	return result;
 }
+
 // Get Result 3
 function getResult3(result3, x, y){
 	var result = parseInt(x*y);
 	result3.val(result); 
 }
 
-});	
+function setTotal(){
+	var totalResult1 = 0, 
+			totalResult2 = 0,
+			totalResult3 = 0;
+
+	$('.result1').each(function() {
+		 	totalResult1 += parseInt($(this).val());
+	});
+	
+	$('.result2').each(function() {
+		 	totalResult2 += parseInt($(this).val());
+	});
+	
+	$('.result3').each(function() {
+		 	totalResult3 += parseInt($(this).val());
+	});
+	
+	// Set Values
+	$('.total-travel .totalResult1').val(totalResult1);
+	$('.total-travel .totalResult2').val(totalResult2);
+	$('.total-travel .totalResult3').val(totalResult3);
+
+	// Set Slider 
+	$('#sl_total_travel').mbsetVal(totalResult1);
+
+}
+
+})(jQuery);
