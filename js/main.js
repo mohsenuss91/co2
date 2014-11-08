a6190759346973a6a27d71f2ae92cb97dfbf2f8c
kossa <hadjikouceyla@gmail.com>
2014-11-08 20:30:08 +0100

    Input + Output from json file

--- js/main.js
+++ js/main.js
@@ -28,7 +28,7 @@
       $('#'+id).mbsetVal(_this.val());
     });
 
-    // on Change Value car
+    // on Change Value 
     $('.setVal input').on('change keyup input', function(event) {
       event.preventDefault();
       var _this = $(this);
@@ -44,27 +44,48 @@
 */
 function calculate(_this){
   var
-  // Get the line
-    theLine = _this.closest("section"),
+      theLine = _this.closest("section"), // Get the section of the element
+      theVal  = theLine.find('.setVal input').val(),
+      set     = theLine.find("input.val").val(),
+      _name   = theLine.data("name"),
+      result1 = theLine.find('.result1'),
+      result2 = theLine.find('.result2'),
+      result3 = theLine.find('.result3');
 
-    // Get all data
-    data1 = calcOBJ['car']["Constants"]["data1"],
-    data2 = calcOBJ['car']["Constants"]["data2"],
-    data3 = calcOBJ['car']["Constants"]["data3"],
-    data4 = calcOBJ['car']["Constants"]["data4"],
-    data5 = calcOBJ['car']["Constants"]["data5"],
-    data6 = calcOBJ['car']["Constants"]["data6"],
-    theVal = theLine.find('.setVal input').val(),
+
+    try{ // Create all Constants
+      var 
+        _Constants = calcOBJ[_name]["Constants"],
+        _Outputs   = calcOBJ[_name]["Outputs"];
+
+      for(var _Constant in _Constants){
+         window[_Constant] = _Constants[_Constant] ;
+      }
+
+      // Get the results
+      for(var _Output in _Outputs){
+         window[_Output] = parseInt(eval(_Outputs[_Output])) ;
+      }
+
+      //Set result
+      result1.val(co2);
+      result2.val(kw);
+      result3.val(participation);
+
+
+    }catch(e){
+      $('#the-error').html("Not declared in the database");
+      return;
+    }
+    
 
     // Get results
-    result1 = theLine.find('.result1'),
-    result2 = theLine.find('.result2'),
-    result3 = theLine.find('.result3');
+    
 
-    // Get Results
+    /* Get Results
     getResult1(result1, theVal ,data1, data2, data3);   
     var result = getResult2(result2, theVal, data1, data3, data5);    
-    getResult3(result3, result ,data6);   
+    getResult3(result3, result ,data6);   */
 
     // Set the Total
     setTotal();
